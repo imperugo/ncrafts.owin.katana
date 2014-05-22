b4784073503741c17e61e0aeaa362bf9d26fb00f
Simone Chiaretta <simone@piyosailing.com>
2014-05-22 15:45:42 +0200

    Fixed tabs

--- 05.OwinMiddlewares/LoggerMiddleware.cs
+++ 05.OwinMiddlewares/LoggerMiddleware.cs
@@ -5,16 +5,17 @@
 	using Microsoft.Owin;
 
 	public class LoggerMiddleware : OwinMiddleware
-    {
-        public LoggerMiddleware(OwinMiddleware next) : base(next)
-        {
-        }
-
-        public override async Task Invoke(IOwinContext context)
-        {
-            await context.Response.WriteAsync("Before (middleware with Katana middleware base class)\r\n");
-            await this.Next.Invoke(context);
-            await context.Response.WriteAsync("After (middleware with Katana middleware base class)\r\n");
-        }
+	{
+	    public LoggerMiddleware(OwinMiddleware next) : base(next)
+	    {
+	        
+	    }
+	    
+	    public override async Task Invoke(IOwinContext context)
+	    {
+	        await context.Response.WriteAsync("Before (middleware with Katana middleware base class)\r\n");
+	        await this.Next.Invoke(context);
+	        await context.Response.WriteAsync("After (middleware with Katana middleware base class)\r\n");
+	    }
     }
 }
